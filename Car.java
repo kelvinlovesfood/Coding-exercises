0b37c3c4114950db31c2134afcabf6faec331777
kelvinlovesfood <s3899733@student.rmit.edu.au>
2021-05-16 20:36:24 +1000

    exercise 3

--- Car.java
+++ Car.java
@@ -1,6 +1,7 @@
 public class Car {
    private int modelYear; 
    // TODO: Declare purchasePrice field (int)
+   private int purchasePrice;
 
    private int currentValue;
    
@@ -13,9 +14,14 @@ public class Car {
    }
    
    // TODO: Define setPurchasePrice() method
+   public void setPurchasePrice(int userPrice) {
+      purchasePrice = userPrice;
+   }
    
    // TODO: Define getPurchasePrice() method
-   
+   public int getPurchasePrice() {
+      return purchasePrice;
+   }
    
    public void calcCurrentValue(int currentYear) {
       double depreciationRate = 0.15;
@@ -27,5 +33,11 @@ public class Car {
    }
    
    // TODO: Define printInfo() method to output modelYear, purchasePrice, and currentValue
+   public void printInfo() {
+      System.out.println("Car's information:");
+      System.out.println("   Model year: " + getModelYear());
+      System.out.println("   Purchase Price: " + getPurchasePrice());
+      System.out.println("   Current Value: " + currentValue);
+   }
    
 }
